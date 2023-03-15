2c171cd0970f6720dbd47cec6f11c6c9dbd888f8
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-15 12:51:41 +0000

    created all components and added routes to pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import Contact from './Components/Contact';
+import Gallery from './Components/ProjectGallery';
+import Home from './Components/Home';
+import Layout from './Components/Layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <></>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Gallery />} />
+          <Route path="contact" element={<Contact />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

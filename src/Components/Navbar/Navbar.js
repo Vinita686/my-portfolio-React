98ba1f543a891820436aa91f83c683a659a96bad
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-16 12:55:31 +0000

    added hamburger click function

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,17 +1,21 @@
 import "./NavbarStyle.css";
 
-import React from 'react'
+import React, {useState }from 'react'
 import { Link } from "react-router-dom";
 
-import { FaBars } from "react-icons/fa"
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+
   return (
     <div className="header">
       <Link to="/">
         <h1>Vinita Yadav</h1>
       </Link>
-      <ul className="nav-items">
+      <ul className={click ? "nav-items active" : "nav-items"}>
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -25,9 +29,16 @@ const Navbar = () => {
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
-        <div className="hamburger">
-          <FaBars size={20} style={{ color: "#fff"}} 
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (
+            <FaTimes size={20} style={{ color: "#fff"}} 
           />
+          ) : (
+            <FaBars size={20} style={{ color: "#fff"}} 
+          />
+          )}
+          
+          
         </div>
 
     </div>

04f4ce6ed7edca0819b1603ce99b8b750fd416ae
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-19 14:41:58 +0000

    added hashrouter instead of browserrouter

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
   
-   <BrowserRouter>
+   <HashRouter>
+    <div>
       <Routes>
-        <Route path='/my-portfolio-react' element={<Home />} />
-          <Route path='/my-portfolio-react/' element={<Home />} />
-          <Route path="/my-portfolio-react/projects" element={<Projects />} />
-          <Route path="/my-portfolio-react/contact" element={<Contact />} />
-          <Route path="/my-portfolio-react/about" element={<About />} />
-          {/* <Route path="*" element={<NoPage />} /> */}
-        
-      </Routes>
-    </BrowserRouter>
+        <Route path='/' element={<Home />} />
+          
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+         </Routes>
+      </div>
+    </HashRouter>
   
   );
 }

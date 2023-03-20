7c4905371e0da58f2ac24a2f7dbd41be5acf51e7
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-20 13:23:54 +0000

    added target blank to run livelink properly

--- src/Components/ProjectGallery/Project.js
+++ src/Components/ProjectGallery/Project.js
@@ -21,7 +21,7 @@ const Project = () => {
             title={item.title} 
             text={item.text} 
             repo={item.repo} 
-            liveLink={item.liveLink} />
+            liveLink={item.liveLink} target="_blank"/>
         )
     )}
     </div>
